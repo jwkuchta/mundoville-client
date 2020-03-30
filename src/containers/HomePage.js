4f5444773dad5a4e181d66cff49d596f1dec6c16
Justina Kuchta <justyna.kuchta@gmail.com>
2020-03-30 15:28:18 -0400

    testing if app updated

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -25,11 +25,11 @@ const HomePage = () => {
                 /><br></br>
             </Container>
             <Container fluid className='main-page-description'>
-                <h3>It's just like couch surfing</h3>
+                {/* <h3>It's just like couch surfing</h3>
                 <h3>except for the creepy factor</h3>
                 <h3>'cause no couches are involved</h3>
                 <h3>don't just be a tourist</h3>
-                <h3>hang out with locals while you travel</h3>
+                <h3>hang out with locals while you travel</h3> */}
             </Container>
             <LoginModal />
         </Container>
