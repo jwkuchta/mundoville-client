5f6b0c37cc28109d680ca31fbf7cced272c98a35
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-03 19:12:12 -0400

    correction

--- src/components/BlogButton.js
+++ src/components/BlogButton.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
-const button = () => {
-    return (
-        <div>
-            <button>Button</button>
-        </div>
-    )
-}
+// const button = () => {
+//     return (
+//         <div>
+//             <button>Button</button>
+//         </div>
+//     )
+// }
 
-export default button
+// export default button
+
+
+// return (
+//     <StyleRoot>
+//         <div className="App">
+//             <h1>Hey, I'm the App componnet</h1>
+//             <p className={classes.join( ' ' )}>Working!</p>
+//             <button
+//               style={style}
+//               onClick={togglePuppiesHandler}>Toggle Puppies
+//             </button>
+//   {puppies}
+//         </div>
+//   </StyleRoot>
+//       )
