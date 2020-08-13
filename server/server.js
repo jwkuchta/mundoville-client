0257f24c27933b75b416eab388e546f98ff71115
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-13 16:17:23 -0400

    changed app config to redirect to api

--- server/server.js
+++ server/server.js
@@ -20,14 +20,22 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+// app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
+
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')))
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
-});
+})
+
+app.get('/api/v1/users', (req, res) => {
+  res.redirect('http://mundoville-api.herokuapp.com/api/v1/users')
+})
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+})
+
 app.listen(port);
